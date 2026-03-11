ebc670fcfaae783ece0ac90893a04fbc7818705c
Samuel Kwota <Sfuturemygamesworld@outlook.com>
2026-03-11 22:12:32 +0000

    New update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,44 +1,48 @@
-
 import express from "express"
 import multer from "multer"
 import pool from "../config/db.js"
 
-const router=express.Router()
+const router = express.Router()
 
-const storage=multer.diskStorage({
-destination:(req,file,cb)=>cb(null,"public/uploads"),
-filename:(req,file,cb)=>cb(null,Date.now()+"-"+file.originalname)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "public/uploads"),
+  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
 })
 
-const upload=multer({storage})
+const upload = multer({ storage })
 
-function checkAuth(req,res,next){
-if(!req.session.admin) return res.redirect("/auth/login")
-next()
+function checkAuth(req, res, next) {
+  if (!req.session.admin) return res.redirect("/auth/login")
+  next()
 }
 
-router.get("/dashboard",checkAuth,async(req,res)=>{
-const cars=await pool.query("SELECT * FROM vehicles ORDER BY id DESC")
-res.render("dashboard",{cars:cars.rows})
+router.get("/dashboard", checkAuth, async (req, res) => {
+  const cars = await pool.query("SELECT * FROM vehicles ORDER BY id DESC")
+  res.render("dashboard", { cars: cars.rows })
 })
 
-router.post("/add",checkAuth,upload.single("image"),async(req,res)=>{
-const {make,model,year,price}=req.body
-const image="/uploads/"+req.file.filename
+router.post("/add", checkAuth, upload.single("image"), async (req, res) => {
 
-const cleanPrice = price.replace(/,/g, "");
+  const { make, model, year, price, mileage } = req.body
 
-await pool.query(
-"INSERT INTO vehicles (make,model,year,price,mileage,image) VALUES ($1,$2,$3,$4,$5,$6)",
-[make,model,year,price.replace(/,/g,''),mileage,image]
-)
+  const cleanPrice = price.replace(/,/g, "")
 
-res.redirect("/admin/dashboard")
+  let image = null
+  if (req.file) {
+    image = "/uploads/" + req.file.filename
+  }
+
+  await pool.query(
+    "INSERT INTO vehicles (make,model,year,price,mileage,image) VALUES ($1,$2,$3,$4,$5,$6)",
+    [make, model, year, cleanPrice, mileage, image]
+  )
+
+  res.redirect("/admin/dashboard")
 })
 
-router.post("/delete/:id",checkAuth,async(req,res)=>{
-await pool.query("DELETE FROM vehicles WHERE id=$1",[req.params.id])
-res.redirect("/admin/dashboard")
+router.post("/delete/:id", checkAuth, async (req, res) => {
+  await pool.query("DELETE FROM vehicles WHERE id=$1", [req.params.id])
+  res.redirect("/admin/dashboard")
 })
 
 export default router
